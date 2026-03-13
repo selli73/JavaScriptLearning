fcba9f0ff77a5d0bb3baa3ae2b43ad3d9750a3c1
Bulat <aabulath@yandex.ru>
2026-03-13 15:32:29 +0400

    add instruction

--- Lesson3/index.js
+++ Lesson3/index.js
@@ -1,1 +1,6 @@
-// check
+// git config --global user.email "test@gmail.com"
+// git config --global user.name "TestName"
+// 1. git init
+// 2. git add .
+// 3. git commit -m "init"
+// 4. git push
